840442b8526f995371d13ba45df86a487024bceb
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-15 01:29:32 +0100

    package

--- functions/src/functions/dogWalkers.js
+++ functions/src/functions/dogWalkers.js
@@ -1,27 +1,20 @@
 const { app } = require('@azure/functions');
 
 app.http('dogWalkers', {
-    module.exports = async function (context, req) {
-        context.log('HTTP trigger function processed a request.');
-      
-        if (req.method === 'POST') {
-          const name = req.body.name;
-          const email = req.body.email;
-          const message = req.body.message;
-      
-          // Process the form data (e.g., validation, storage)
-          console.log(`Received form data: Name: ${name}, Email: ${email}, Message: ${message}`);
-      
-          context.res = {
-            // ... (optionally send a response)
-          };
-        } else {
-          context.res = {
-            status: 405,
-            body: "This function only accepts POST requests."
-          };
-        }
-      };
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const yourname = request.query.get('yourname') || await request.text() || 'No name supplied';
+        const email = request.query.get('email') || await request.text() || 'No email supplied';
+        const town = request.query.get('town') || await request.text() || 'No town supplied';
+        const postcode = request.query.get('postcode') || await request.text() || 'No postcode supplied';
+
+
+        return { body: `Hello, ${yourname}, we've recieved your request!` };
+
+    }
 });
 
 
