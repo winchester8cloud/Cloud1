6bacf62b6950934d27d303baf253bf695bf1f986
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-15 01:27:36 +0100

    new

--- functions/src/functions/dogWalkers.js
+++ functions/src/functions/dogWalkers.js
@@ -1,23 +1,27 @@
-module.exports = async function (context, req) {
-    context.log('HTTP trigger function processed a request.');
-  
-    if (req.method === 'POST') {
-      const yourname = req.body.yourname;
-      const email = req.body.email;
-      const town = req.body.postcode;
-  
-      // Process the form data (e.g., validation, storage)
-      console.log(`Received form data: Name: ${yourname}, Email: ${email}, Message: ${postcode}`);
-  
-      context.res = {
-        // ... (optionally send a response)
+const { app } = require('@azure/functions');
+
+app.http('dogWalkers', {
+    module.exports = async function (context, req) {
+        context.log('HTTP trigger function processed a request.');
+      
+        if (req.method === 'POST') {
+          const name = req.body.name;
+          const email = req.body.email;
+          const message = req.body.message;
+      
+          // Process the form data (e.g., validation, storage)
+          console.log(`Received form data: Name: ${name}, Email: ${email}, Message: ${message}`);
+      
+          context.res = {
+            // ... (optionally send a response)
+          };
+        } else {
+          context.res = {
+            status: 405,
+            body: "This function only accepts POST requests."
+          };
+        }
       };
-    } else {
-      context.res = {
-        status: 405,
-        body: "This function only accepts POST requests."
-      };
-    }
-  };
+});
 
 
