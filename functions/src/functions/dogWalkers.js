89031c87f6e675f5555f98544816530384d6b679
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-15 16:45:31 +0100

    Changed func.json

--- functions/src/functions/dogWalkers.js
+++ functions/src/functions/dogWalkers.js
@@ -1,3 +1,26 @@
+const { app, output } = require('@azure/functions');
+const crypto = require('crypto');
+const mssql = require('mssql');
+
+const config = {
+  server: 'admin-waggly.database.windows.net',
+  user: 'server-admin-waggly',
+  password: 'Wag881!!',
+  database: 'waggly',
+  pool: { 
+    max: 100, 
+    min: 0, 
+    idleTimeoutMillis: 50000 
+
+  }, 
+  authentication: {
+    type: 'default'
+  },
+  options: {
+      encrypt: true
+  }
+};
+
 const { app } = require('@azure/functions');
 
 app.http('dogWalkers', {
@@ -6,8 +29,38 @@ app.http('dogWalkers', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        if (req.method === 'POST') {
+          const body = req.body;
+      
+          if (body) {
+            try {
+              const { name, email, town, postcode } = body;
+      
+              // You might want to add data validation here
+      
+              // Replace with your actual database connection logic (e.g., using a SQL library)
+              const response = await addWalkerToDatabase(name, email, town, postcode);
+              return { response };}
+            catch {
+              return {body: "Database function didn't run."};
+            }
+          }
 
-        return { body: `Hello, ${name}!` };
     }
-});
+}});
+
+const addWalkerToDatabase = async (name, email, town, postcode) => { 
+  try { 
+    const pool = await sql.connect(config); 
+    const result = await pool.request() 
+      .input('name', sql.NVarChar, name) 
+      .input('email', sql.NVarChar, email) 
+      .input('town', sql.NVarChar, town) 
+      .input('postcode', sql.NVarChar, postcode) 
+      .query('INSERT INTO [dbo].[dogWalkers] (name, email, town, postcode) VALUES (name, @email, @town, @postcode);'); 
+    return { body: 'Your information has been successfully submitted!' };
+  } catch (err) { 
+    console.log(err); 
+    return { body: 'Your information has not been successfully submitted, please try again!' };
+  } 
+}
