5c0841ab028b2b5ceabbb6546c411ddd2d39ea17
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-16 23:45:22 +0100

    try

--- pythonFunctions/function_app.py
+++ pythonFunctions/function_app.py
@@ -3,6 +3,8 @@ from azure.functions.decorators.core import DataType
 import json
 import uuid
 import logging
+from sqlalchemy import create_engine
+from func import ConnectionStringConfig
 
 app = func.FunctionApp()
 
@@ -18,6 +20,17 @@ def dogWalkersPython(req: func.HttpRequest, dogWalkerInfo: func.Out[func.SqlRow]
 
     if name:
         try:
+            # Retrieve connection string from Azure App Settings
+            sql_connection_string = ConnectionStringConfig().get_connection_string("SqlConnectionString")
+            engine = create_engine(sql_connection_string)
+
+      # Execute insert query with SQLAlchemy (parameterized for security)
+            engine.execute(
+            db.dogWalkers.insert(),
+            name=name,
+            email=email,
+            town=town,
+            postcode=postcode)
             dogWalkerInfo.set(func.SqlRow({"name": name, "email": email, "town": town, "postcode": postcode}))
             return func.HttpResponse(f"Hello, {name}. You've been registered successfully!.")
         except Exception as e:
