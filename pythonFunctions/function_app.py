fc380c41df0b69bde8c58b78b922f67baf699cce
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-17 00:20:48 +0100

    work

--- pythonFunctions/function_app.py
+++ pythonFunctions/function_app.py
@@ -7,7 +7,7 @@ import logging
 app = func.FunctionApp()
 
 @app.route(route="pythonDogWalkers", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET","POST"])
-@app.generic_output_binding(arg_name="dogWalkerInfo", type="sql", CommandText="dbo.dogWalkers", ConnectionStringSetting="SqlConnectionString", data_type=DataType.STRING)
+@app.generic_output_binding(arg_name="dogWalkerInfo", type="sql", CommandText="dbo.dogWalkers", ConnectionStringSetting="Server=tcp:admin-waggly.database.windows.net,1433;Initial Catalog=waggly;Persist Security Info=False;User ID=server-admin-waggly;Password=Wag881!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", data_type=DataType.STRING)
 def dogWalkersPython(req: func.HttpRequest, dogWalkerInfo: func.Out[func.SqlRow]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -18,10 +18,8 @@ def dogWalkersPython(req: func.HttpRequest, dogWalkerInfo: func.Out[func.SqlRow]
 
     if name:
         try:
-            #dogWalkerInfo.set(func.SqlRow({"name": name, "email": email, "town": town, "postcode": postcode}))
-            #rows = func.SqlRowList({"name": name, "email": email, "town": town, "postcode": postcode})
-            #dogWalkerInfo.set(rows)
-            return func.HttpResponse(f"Hello, {name}. You've been registered successfully!.")
+            dogWalkerInfo.set(func.SqlRow({"name": name, "email": email, "town": town, "postcode": postcode}))
+            return func.HttpResponse(f"Hello, {name}. You've been registered successfully!")
         except:
             return func.HttpResponse(f"Hello, {name}. Please try again, your information wasn't passed to our database!")
     else:
