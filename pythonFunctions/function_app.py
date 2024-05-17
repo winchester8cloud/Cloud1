a7a5245a6fd635cf1c450c0a5c4b7d1aa7faef09
winchester8cloud <l.smith8.22@unimail.winchester.ac.uk>
2024-05-17 15:19:01 +0100

    New function

--- pythonFunctions/function_app.py
+++ pythonFunctions/function_app.py
@@ -25,8 +25,17 @@ def dogWalkersPython(req: func.HttpRequest, dogWalkerInfo: func.Out[func.SqlRow]
         connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
         conn = pyodbc.connect(connectionString)
         SQL_QUERY = """
-                INSERT INTO dbo.dogWalkers (name, email, town, postcode)
-                VALUES (?,?,?,?);
+                USE [waggly]
+                GO
+
+                INSERT INTO [dbo].[dogWalkers]
+                        ([NAME]
+                        ,[EMAIL]
+                        ,[TOWN]
+                        ,[POSTCODE])
+                    VALUES
+                        (?, ?, ?, ?)
+                GO
                 """
         cursor = conn.cursor()
         cursor.execute(SQL_QUERY, (name, email, town, postcode))
